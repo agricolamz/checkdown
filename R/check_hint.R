d70fd312d2226b7b8c6a9c573ede7811855fe16a
George Moroz <agricolamz@gmail.com>
2020-04-13 16:04:54 +0300

    add check_hints

--- R/check_hint.R
+++ R/check_hint.R
@@ -1,7 +1,7 @@
 #' Create check-fields and check-boxes for 'rmarkdown'
 #'
 #' @param hint_text hint paragraph text; can contain markdown
-#' @param click_text click paragraph text; can contain markdown
+#' @param hint_title click paragraph text; can contain markdown
 #' @param hint_id unique identidier of the question
 #'
 #' @return returns the html and javascript code
@@ -20,24 +20,25 @@
 #'
 
 check_hint <- function(hint_text,
-                       click_text = "Click here to see/close the hint",
+                       hint_title = "Click here to see/close the hint",
                        hint_id = sample(1:1e5, 1)){
   if(knitr::is_html_output()){
     hint_text <- (markdown::markdownToHTML(text = hint_text,
                                            output = NULL,
                                            fragment.only = TRUE))
     hint_text <- gsub("(<.?p>)|(\n)|(\\#)", "", hint_text)
-    click_text <- (markdown::markdownToHTML(text = click_text,
+    hint_title <- as.character(hint_title)
+    hint_title <- (markdown::markdownToHTML(text = hint_title,
                                             output = NULL,
                                             fragment.only = TRUE))
-    click_text <- gsub("(<.?p>)|(\n)|(\\#)", "", click_text)
+    hint_title <- gsub("(<.?p>)|(\n)|(\\#)", "", hint_title)
 
     cat(paste0(c('<p id="hint_',
                  hint_id,
                  '", onclick="return show_hint_',
                  hint_id,
                  '()">',
-                 click_text,
+                 hint_title,
                  '</p><p id="result_',
                  hint_id,
                  '"></p>',
@@ -56,7 +57,7 @@ check_hint <- function(hint_text,
                  hint_id,
                  '").innerHTML = "";}}',
                  '</script>'),
-                 collapse = ""
-                 ))
+               collapse = ""
+    ))
   }
 }
---
+++ R/check_hints.R
@@ -0,0 +1,107 @@
+#' Create check-fields and check-boxes for 'rmarkdown'
+#'
+#' @param hint_text hint paragraph texts; can contain markdown
+#' @param hint_title hint title texts; can contain markdown
+#' @param hint_id unique identidier for each hint
+#' @param list_title unique identidier for each hint
+#'
+#' @return returns the html and javascript code
+#'
+#' @author George Moroz <agricolamz@gmail.com>
+#' @examples
+#'
+#' # ```{r, results='asis', echo=FALSE}
+#' # check_hints(1:4)
+#' # ```
+#'
+#' @export
+#'
+#' @importFrom knitr is_html_output
+#' @importFrom markdown markdownToHTML
+#'
+
+check_hints <- function(hint_text,
+                        hint_title = "Click here to see/close the hint",
+                        list_title = "Click here to see/close the list of hints",
+                        hint_id){
+  if(knitr::is_html_output()){
+    df <- data.frame(hint_text, hint_title, stringsAsFactors = FALSE)
+    df$hint_id <- sample(2:1e5, nrow(df))
+
+    df$hint_text <- unlist(lapply(seq_along(df$hint_text), function(i){
+      x <- as.character(df$hint_text[i])
+      x <- (markdown::markdownToHTML(text = x,
+                                     output = NULL,
+                                     fragment.only = TRUE))
+      x <- gsub("(<.?p>)|(\n)|(\\#)", "", x)
+      x
+    }))
+
+    df$hint_title <- unlist(lapply(seq_along(df$hint_title), function(i){
+      x <- as.character(df$hint_title[i])
+      x <- (markdown::markdownToHTML(text = x,
+                                     output = NULL,
+                                     fragment.only = TRUE))
+      x <- gsub("(<.?p>)|(\n)|(\\#)", "", x)
+      x
+    }))
+
+    list_title <- (markdown::markdownToHTML(text = list_title,
+                                            output = NULL,
+                                            fragment.only = TRUE))
+    list_title <- gsub("(<.?p>)|(\n)|(\\#)", "", list_title)
+
+    hints <- paste0('<p id="hint_',
+                    df$hint_id,
+                    '", onclick="return show_hint_',
+                    df$hint_id,
+                    '()">',
+                    df$hint_title,
+                    '</p><p id="result_',
+                    df$hint_id,
+                    '"></p>',
+                    collapse = "")
+
+    cat(paste0(c('<p id="hint_',
+                 1,
+                 '", onclick="return show_hint_',
+                 1,
+                 '()">',
+                 list_title,
+                 '</p><p id="result_',
+                 1,
+                 '"></p>',
+                 '<script> function show_hint_',
+                 1,
+                 '() {',
+                 'var x = document.getElementById("result_',
+                 1,
+                 '").innerHTML;',
+                 "if(!x){document.getElementById('result_",
+                 1,
+                 "').innerHTML = '",
+                 hints,
+                 "';}",
+                 'else {document.getElementById("result_',
+                 1,
+                 '").innerHTML = "";}}',
+                 paste0(
+                 'function show_hint_',
+                 df$hint_id,
+                 '() {',
+                 'var x = document.getElementById("result_',
+                 df$hint_id,
+                 '").innerHTML;',
+                 "if(!x){document.getElementById('result_",
+                 df$hint_id,
+                 "').innerHTML = '",
+                 df$hint_text,
+                 "';}",
+                 'else {document.getElementById("result_',
+                 df$hint_id,
+                 '").innerHTML = "";}}',
+                 collapse = ""),
+                 '</script>'),
+               collapse = ""))
+  }
+}
